2b1cdc0a00328af299b4609bfb801409ced0f3d4
vanaigr <vanaigranov@gmail.com>
2025-07-24 17:49:10 -0500

    feat: arbitrary data directory

--- src/conf.ts
+++ src/conf.ts
@@ -1,7 +1,9 @@
 import path from 'node:path'
 import config from '../config.ts'
 
-export const root = path.join(import.meta.dirname, '..')
-export const data = path.join(root, 'data')
+export const data = path.resolve(
+    path.join(import.meta.dirname, '..'),
+    config.dataDirectory
+)
 
 export { config }
